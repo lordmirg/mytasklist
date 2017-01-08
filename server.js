01cae427223b6f93cb193c363d36279bef18de2e
lordmirg <grimmy.efg@gmail.com>
2017-01-08 17:37:44 +0000

    Added Angular 2 skeleton

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var port = 3000;
 var app = express();
 
 // View Engine
-
+console.log('Dirname' + __dirname);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
